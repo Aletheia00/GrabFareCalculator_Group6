9005aa1ab924a9f734d0d2fc83e193ca981441b1
wingjinn <trisngwj@gmail.com>
2025-09-23 15:58:38 +0800

    Input for distance and ride stat time

--- FareCalculator.cpp
+++ FareCalculator.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "Grab Fare Calculator Project" << endl;
+    double distance;
+    int time;
+
+    cout << "==============================" << endl;
+    cout << "   Grab Fare Calculator" << endl;
+    cout << "==============================" << endl;
+
+    cout << "Enter distance (km): ";
+    cin >> distance;
+
+    cout << "Enter ride start time (0 - 23 hours): ";
+    cin >> time;
+
     return 0;
 }
-
